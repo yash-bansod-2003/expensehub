798dcedddd2fa4e442f9b73c294f98231be60454
= <yashbansodgit@gmail.com>
2024-10-21 00:51:12 +0530

    refactor: edit main body title

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
       <img alt="logo" className="logo" src={electronLogo} />
       <div className="creator">Powered by electron-vite</div>
       <div className="text">
-        Build an Electron app with <span className="react">React</span>
+        Build an ExpenseHub Electron app with <span className="react">React</span>
         &nbsp;and <span className="ts">TypeScript</span>
       </div>
       <p className="tip">
