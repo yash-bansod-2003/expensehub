c1cd3c41777a43fbcf03b91dd6fd8f73594bf3b7
= <yashbansodgit@gmail.com>
2024-11-02 00:04:26 +0530

    feat: toggle dark mode

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,10 +2,16 @@ import './assets/main.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from '@renderer/components/ui/sonner'
+import { ThemeProvider } from '@renderer/components/theme-provider'
 import App from './App'
+import '@fontsource-variable/inter'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      <Toaster />
+    </ThemeProvider>
   </React.StrictMode>
 )
